58347eec8a072748e3ef3d4c267f9f6831db6904
bf4r <sharpslpcz@gmail.com>
2025-01-30 17:52:06 +0100

    add death and killing

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
     static void Main(string[] args)
     {
         var logger = new Logger();
-        Thing thing = new Thing("Thing", "Just a thing.");
+        Thing thing = new Thing("Thing", "A thing.");
         logger.Log(LogLevel.Info, $"A thing has been created, its name is {thing.Name ?? "unknown"}.");
         if (thing.Description != null)
         {
@@ -23,10 +23,27 @@ class Program
         Person person = new Person();
         person.Conceive();
         person.Birth();
+        person.ReassignGender(Gender.Female);
         person.GiveName("Eve", "Smith");
 
         logger.Log(LogLevel.Info, $"A new baby was born! Their name is {person.GetFullName() ?? "Unknown name"} and they are {person.GetAgeYears()} years old.");
         logger.Log(LogLevel.Info, "Person details: " + person.GetDetails());
+
+        Person person2 = new Person();
+        person2.Conceive();
+        person2.Birth();
+        person2.ReassignGender(Gender.Male);
+        person2.GiveName("Jonathan", "Lee");
+
+        logger.Log(LogLevel.Info, $"A new baby was born! Their name is {person2.GetFullName() ?? "Unknown name"} and they are {person2.GetAgeYears()} years old.");
+
+        logger.Log(LogLevel.Info, "There has been a murder.");
+        logger.Log(LogLevel.Info, "Killer details: " + person2.GetDetails());
+        logger.Log(LogLevel.Info, "Victim details: " + person.GetDetails());
+
+        person2.Kill(person);
+        logger.Log(LogLevel.Info, "The victim is now " + (person.IsDead ? "dead" : "alive") + ".");
+        logger.Log(LogLevel.Info, "The killer is " + person.Killer?.GetDetails() + ".");
 
         logger.PrintLogs();
     }
--- Things/Society/Person.cs
+++ Things/Society/Person.cs
@@ -3,6 +3,7 @@ using System.Text;
 
 public class Person : Thing
 {
+    // Identity
     public string? FirstName { get; private set; }
     public string? LastName { get; private set; }
     public string? GetFullName()
@@ -18,16 +19,8 @@ public class Person : Thing
         return sb.ToString();
     }
 
+    // Gender
     public Gender Gender { get; set; }
-
-    public DateTime? Conceived { get; private set; }
-    public DateTime? Born { get; private set; }
-
-    public bool IsConceived => Conceived != null;
-    public bool IsBorn => Born != null;
-
-    public Person() : base("Person", "A person.") { }
-
     public string GetDetails()
     {
         string? fullName = GetFullName();
@@ -39,16 +32,21 @@ public class Person : Thing
         return $"Unnamed Person ({age}{Gender.GetOneLetterAbbreviation()})";
     }
 
+    // Conception
+    public DateTime? Conceived { get; private set; }
     public void Conceive()
     {
-        if (Conceived != null) throw new Exception("The person has already been conceived.");
+        if (IsConceived) throw new Exception("The person has already been conceived.");
         Conceived = DateTime.Now;
     }
+
+    // Birth and age
+    public DateTime? Born { get; private set; }
     public void Birth()
     {
         Random random = new();
-        if (Conceived == null) throw new Exception("The person needs to have been conceived in order to be born.");
-        if (Born != null) throw new Exception("The person has already been born.");
+        if (!IsConceived) throw new Exception("The person needs to have been conceived in order to be born.");
+        if (IsBorn) throw new Exception("The person has already been born.");
 
         Born = DateTime.Now;
 
@@ -70,6 +68,46 @@ public class Person : Thing
         if (ageTimeSpan == null || !IsBorn) throw new Exception("The person has not been born yet.");
         return (int)(Math.Floor(ageTimeSpan.Value.TotalDays / 365.0));
     }
+
+    // Death
+    public DateTime? Died { get; private set; }
+    public string? CauseOfDeath { get; private set; }
+    public Person? Killer { get; private set; }
+
+    public void Die(string? causeOfDeath)
+    {
+        if (IsDead) throw new Exception("The person is already dead.");
+        if (!IsConceived) throw new Exception("The person has not been conceived yet.");
+        CauseOfDeath = causeOfDeath;
+        Died = DateTime.Now;
+        Killer = null;
+    }
+
+    public void Die()
+    {
+        if (IsDead) throw new Exception("The person is already dead.");
+        if (!IsConceived) throw new Exception("The person has not been conceived yet.");
+        CauseOfDeath = null;
+        Killer = null;
+        Died = DateTime.Now;
+    }
+
+    public void Kill(Person person)
+    {
+        if (IsDead) throw new Exception("The person is already dead.");
+        if (!IsConceived) throw new Exception("The person has not been conceived yet.");
+        if (person.Id == this.Id) CauseOfDeath = "Suicide";
+        person.CauseOfDeath = null;
+        person.Killer = this;
+        person.Died = DateTime.Now;
+    }
+
+    public bool IsConceived => Conceived != null;
+    public bool IsBorn => Born != null;
+    public bool IsDead => Died != null;
+
+    public Person() : base(nameof(Person), $"A {nameof(Person).ToLower()}.") { }
+
     public void GiveName(string? firstName = null, string? lastName = null)
     {
         FirstName = firstName;
