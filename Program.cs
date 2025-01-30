20cc0ecb4ccfb08e8e33b9c6185af87b497b1d44
bf4r <sharpslpcz@gmail.com>
2025-01-30 15:18:08 +0100

    add thing class and example program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Thing thing = new Thing("Thing", "Just a thing.");
+        Console.WriteLine($"A thing has been created, its name is {thing.Name ?? "unknown"}.");
+        if (thing.Description != null)
+        {
+            Console.WriteLine($"More about {thing.Name ?? "the unknown thing"}: {thing.Description}");
+        }
     }
 }
---
+++ Things/Thing.cs
@@ -0,0 +1,14 @@
+// all objects in slap should inherit from this class
+// this allows for general actions and properties in all objects
+public class Thing
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public Thing(string? name = null, string? description = null)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+    }
+}
